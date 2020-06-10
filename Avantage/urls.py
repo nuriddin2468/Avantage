cb36cb01cb792caa96781fdf66ba7d685b54f05b
Nuriddin <nuriddin_24468@mail.ru>
2020-06-11 00:33:07 +0500

    addons

--- Avantage/urls.py
+++ Avantage/urls.py
@@ -14,19 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls'))
 
 ]
 
-if settings.DEBUG:
-    urlpatterns += [
-        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
-    ]
-
-CORS_ORIGIN_WHITELIST = [
-    "127.0.0.1:8000"
-]
