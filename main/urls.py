c1f4e843eed65232cf4b455609e137a5531d1dd8
Nuriddin <nuriddin_24468@mail.ru>
2020-06-14 03:38:46 +0500

    microfix

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from main.views import TagsListView, TagView
 urlpatterns = [
-    path('tag/all/', TagsListView.as_view()),
-    path('tag/<int:pk>/', TagView.as_view())
+    path('tag/all', TagsListView.as_view()),
+    path('tag/<int:pk>', TagView.as_view())
 ]
