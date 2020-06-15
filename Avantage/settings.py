d909d168b0cec7847cc8358cbfa7ea5fa5524f3a
Nuriddin <nuriddin_24468@mail.ru>
2020-06-15 18:43:19 +0500

    cors all

--- Avantage/settings.py
+++ Avantage/settings.py
@@ -136,6 +136,8 @@ STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 CORS_ORIGIN_WHITELIST = [
     "https://modest-bhabha-038ffd.netlify.app",
     "http://127.0.0.1:8000",
